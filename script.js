158e51047f6bc21f8a8ed5932459a4af2c3eb8b2
excell-ent <ayexcellent123@gmail.com>
2025-04-19 23:19:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+
+function cancel(){
+	const sideBar = document.querySelector('.side-bar');
+		sideBar.style.display = 'none';
+}
+function menuDisplay(){
+	const sideBar = document.querySelector('.side-bar');
+	sideBar.style.display = 'flex';
+}
+function nightMode(){
+	if (document.title === 'Xcellent') {
+		document.body.style.background = 'linear-gradient(to bottom right, #332222, rgba(15, 10, 10, 0.781)) no-repeat fixed';
+		document.body.style.color = "white";
+			document.querySelector('footer').style.background= "linear-gradient(to bottom right, #44171f45, #54072354)";
+			document.querySelector('main').style.color = "#ffeeee";
+		document.title = 'Xcellents';
+	} else if (document.title === 'Xcellents') {
+		document.body.style.background = '';
+		document.body.style.color = "";
+		document.body.style.opacity = '1';
+		document.title = 'Xcellent'
+	document.querySelector('footer').style.background = '';
+	}
+
+};
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
